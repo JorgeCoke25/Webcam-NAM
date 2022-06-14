7950748334d829f20145d017b06cf7efa9c04635
Jorge_Barrios <jorge.barriosn@usm.cl>
2022-06-14 10:30:30 -0400

    Verficacion flujo de imagenes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const {Socket} = require("socket.io");
 const app = express();
 
+let contador = 0;
+
 //creamos un servidor http a partir de la libreria de express
 const http = require('http').Server(app);
 //Para generar una comunicacion vamos a trabajar con Socket.io
@@ -16,7 +18,8 @@ app.use(express.static(__dirname+"/public"))
 
 io.on('connection', (socket)=>{
     socket.on('stream', (image)=>{
-        //emitir el evento a todos los sockets
+        //emitir el evento a todos los sockets, aqui tambien se hace la logica de respecto al proceso de imagenes.
+        console.log(contador+=1)
         socket.broadcast.emit('stream',image);
     })
 })
